2bec1e8c03fabc7c83b51135877028f9c6ebf5d9
moxdv777 <moxdv777@mail.ru>
2018-06-21 11:38:57 +0300

    Создал первый проект

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System;
+using System.Linq;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int _hour = DateTime.Now.Hour;
+            ViewBag.Greeting = _hour < 12 ? "Доброе утро!" : "Добрый вечер";
+            return View("MyView");
+        }
+
+        public ViewResult RsvpForm() {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest) {
+            if (ModelState.IsValid) {
+                Repository.AddResponse(guest);
+                return View("Thanks", guest);
+            } else {
+                return View();
+            }
+           
+        }
+
+        public ViewResult ListResponses() {
+            return View(Repository.Responses.Where(g => g.WillAttend == true));
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse {
+        [Required(ErrorMessage = "Пожалуйста, введите свое имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите ваш Email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Не правильно введен Email адрес")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите свой телефон")]
+        [Phone(ErrorMessage = "Не правильно введен номер телефона")]
+        public string Phone { get; set; }
+        
+        [Required(ErrorMessage = "Пожалуйста, укажите, примете ли участие")]
+        public bool? WillAttend { get; set; }
+    }
+}
