52f37ef0507043d6528fda4f97503385eb4e16f3
moxdv777 <moxdv777@mail.ru>
2018-06-21 16:56:56 +0300

    Create 3 project

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var _product = new Product {
+                ID = 1,
+                Name = "Каяк",
+                Description = "Лодка для одного человека",
+                Category = "Водный спорт",
+                Price = 275m
+            };
+            ViewBag.StockLevel = 2;
+            return View(_product);
+        }
+    }
+}
